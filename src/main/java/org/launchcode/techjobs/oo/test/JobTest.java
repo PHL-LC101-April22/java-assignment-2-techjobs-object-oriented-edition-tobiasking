05109c2362387e6066ae24cad9130fbca492b182
toby king <tobyjacquesking@gmail.com>
2022-08-21 18:08:04 -0400

    continuining tests

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -21,4 +21,26 @@ public class JobTest {
         jobTest2 = new Job();
         Assert.assertFalse(jobTest1.getId() == jobTest2.getId());
     }
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job test = new Job("Rocketship", new Employer("LaunchCode"), new Location("In Orbit"), new PositionType("Astronomical"), new CoreCompetency("Fuel Burn-Off"));
+        Assert.assertTrue(test.getName() instanceof String);
+        Assert.assertTrue(test.getEmployer() instanceof Employer);
+        Assert.assertTrue(test.getLocation() instanceof Location);
+        Assert.assertTrue(test.getPositionType() instanceof PositionType);
+        Assert.assertTrue(test.getCoreCompetency() instanceof CoreCompetency);
+
+        Assert.assertEquals("Rocketship", test.getName());
+        Assert.assertEquals("LaunchCode", test.getEmployer().getValue());
+        Assert.assertEquals("In Orbit", test.getLocation().getValue());
+        Assert.assertEquals("Astronomical", test.getPositionType().getValue());
+        Assert.assertEquals("Fuel Burn-Off", test.getCoreCompetency().getValue());
+    }
+    @Test
+    public void testJobsForEquality() {
+        Job jobOne = new Job("Rocketship", new Employer("LaunchCode"), new Location("In Orbit"), new PositionType("Astronomical"), new CoreCompetency("Fuel Burn-Off"));
+        Job jobTwo = new Job("Mars Rover", new Employer("NASA"), new Location("Mars"), new PositionType("On Surface"), new CoreCompetency("Pain"));
+        Assert.assertFalse(jobOne.equals(jobTwo));
+    }
+
 }
