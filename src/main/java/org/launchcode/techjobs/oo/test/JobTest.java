f5680048942fe08d305e22e321a7e50e9f9dbc4a
toby king <tobyjacquesking@gmail.com>
2022-08-21 17:56:02 -0400

    started junit

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,10 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.junit.Assert;
+import org.launchcode.techjobs.oo.*;
 
 /**
  * Created by LaunchCode
@@ -9,4 +12,13 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+    Job jobTest1;
+    Job jobTest2;
+
+    @Test
+    public void testSettingJobId() {
+        jobTest1 = new Job();
+        jobTest2 = new Job();
+        Assert.assertFalse(jobTest1.getId() == jobTest2.getId());
+    }
 }
